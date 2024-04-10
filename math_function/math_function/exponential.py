ae5a645bda15db877dff964b3e0a1bfb55db3053
EVIOLAL9U <leonardo.viola.nttdata@skytv.it>
2024-04-10 17:25:47 +0200

    Progetto math function

--- math_function/math_function/exponential.py
+++ math_function/math_function/exponential.py
@@ -12,3 +12,10 @@ class FunzioneEsponenziale(FunzioneMatematica):
 
     def calcola(self, x):
         return self.base ** x
+
+if __name__ == '__main__':
+    x = input("inserisci i valori da sommare separati da virgola: ")
+    checked=check(x)
+    print("vuoi sommare questi numeri", checked)
+    ris=somma(checked)
+    print(f' Facile! fa {ris}')
