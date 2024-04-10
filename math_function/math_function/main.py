be1a0589f9b4fdc23ac399ef30703f6068beca37
EVIOLAL9U <leonardo.viola.nttdata@skytv.it>
2024-04-10 17:54:56 +0200

    Progetto math function

--- math_function/math_function/main.py
+++ math_function/math_function/main.py
@@ -1,13 +1,16 @@
 import exponential
 
+
 def main():
+
     try:
         numero = int(input("Inserisci un numero intero: "))
         funzione_esponenziale = exponential.FunzioneEsponenziale(numero)
-        risultato = funzione_esponenziale.calcola(2)  # Esempio: calcola la funzione esponenziale con base 2
+        risultato = funzione_esponenziale.calcola(2)  # esponenziale con base 2
         print("Il risultato della funzione esponenziale è:", risultato)
     except ValueError:
         print("Errore: Devi inserire un numero intero.")
+
 
 if __name__ == "__main__":
     main()
