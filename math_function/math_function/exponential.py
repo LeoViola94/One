bf16e1ee49cfa4b04d6d602b5c21debcfad6487e
EVIOLAL9U <leonardo.viola.nttdata@skytv.it>
2024-04-10 17:05:24 +0200

    Progetto math function

---
+++ math_function/math_function/exponential.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+import math
+
+class FunzioneMatematica(ABC):
+    @abstractmethod
+    def calcola(self, x):
+        pass
+
+class FunzioneEsponenziale(FunzioneMatematica):
+    def __init__(self, base):
+        self.base = base
+
+    def calcola(self, x):
+        return self.base ** x
---
+++ math_function/math_function_test/test.py
@@ -0,0 +1,18 @@
+import unittest
+from exponential import FunzioneEsponenziale
+
+
+class TestFunzioneEsponenziale(unittest.TestCase):
+    def test_calcola(self):
+
+        funzione = FunzioneEsponenziale(2)
+
+
+        self.assertEqual(funzione.calcola(0), 1)  # 2^0 = 1
+        self.assertEqual(funzione.calcola(1), 2)  # 2^1 = 2
+        self.assertEqual(funzione.calcola(2), 4)  # 2^2 = 4
+        self.assertEqual(funzione.calcola(3), 8)  # 2^3 = 8
+
+
+if __name__ == "__main__":
+    unittest.main()
